3867821b0985db4a1d8b8086dd36de6f3c5e316c
kmok <kmok@thoughtworks.com>
2020-03-22 11:03:27 +0100

    km: adding validation for env vars

--- get-token.sh
+++ get-token.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+if [ -z "${OKTA_USERNAME}" ]; then
+  echo "OKTA_USERNAME not set. Try exiting this container and rerunning ./go setup"
+  exit 1
+fi
+
+if [ -z "${OKTA_SERVER}" ]; then
+  echo "OKTA_SERVER not set. Try exiting this container and rerunning ./go setup"
+  exit 1
+fi
+
+if [ -z "${OKTA_APPTYPE}" ]; then
+  echo "OKTA_APPTYPE not set. Try exiting this container and rerunning ./go setup"
+  exit 1
+fi
+
+if [ -z "${OKTA_APP_ID}" ]; then
+  echo "OKTA_APP_ID not set. Try exiting this container and rerunning ./go setup"
+  exit 1
+fi
+
+
 oktaauth \
   --username $OKTA_USERNAME \
   --server $OKTA_SERVER \
